6a9014af4103248081c73b5071da347afde8d450
tangtc <tangtc@gdshuyeit.com>
2023-08-06 20:29:50 +0800

    新增v1.1

--- pages/page_2.py
+++ pages/page_2.py
@@ -1,13 +1,10 @@
 import os
 import streamlit as st
-
-
-
-st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# # Page 2 ❄️")
+#左边侧栏的显示
+st.sidebar.markdown("## 逻辑回归 ❄️")
 st.write("Welcome to my 3 space~")
-st.markdown("# 机器学习")
-st.markdown("## 线性回归")
+
+st.markdown("## 1.0线性回归")
 st.latex(r'''X=(x_1,x_2,...,x_n)^T=
          \begin{bmatrix} 
    a & b \\ 
@@ -19,3 +16,25 @@ st.latex(r'''\begin{aligned}{\tilde w}&=\arg \mathop{\max}\limits_{w}\Sigma_{i=1
 &= \arg \mathop{\max}\limits_{w}\Sigma_{i=1}^{n}ln(exp\left\{{-(\frac{(y_i-(w^Tx_i+b))^2}{2\sigma^2})}\right\}) \\ 
 &=\arg \mathop{\min}\limits_{w}\Sigma_{i=1}^{n}\frac{(y_i-(w^Tx_i+b))^2}{2\sigma^2} \\ 
 &\propto \arg\mathop{\min}\limits_{w}\Sigma_{i=1}^{n}(y_i-(w^Tx_i+b))^2\end{aligned} ''')
+
+####################################逻辑回归##########################################
+st.markdown("## 1.1逻辑回归")
+####################################逻辑回归模型######################################
+st.markdown("### 1.1.1 逻辑回归模型")
+st.markdown("&emsp;&emsp;逻辑回归是用来做分类算法的。把线性回归的结果$Y$代入一个非线性变换的$ sigmoid$函数中，\
+      即可得到$[0,1]$之间取值范围的数$S$，$S$可以把它看成是一个概率值，如果设置概率阈值为$0.5$，那么$S$大于$0.5$可以看成是正样本，\
+            小于$0.5$看成是负样本，就可以进行分类。$sigmoid$函数公式如下：")
+st.latex(r'''\begin{aligned}sigmoid(x)=\frac{1}{1+e^{-x}}\end{aligned} \tag {1.1}''')
+st.markdown("&emsp;&emsp;在二分类逻辑回归场景下，\
+            给定模型参数$w_i$和样本参数$x_i$,\
+            那么样本被分类为1的概率为$p(y_i=1|x_i;w)=\cfrac{1}{1+e^{-w^T{x_i}}}=\cfrac{e^{w^Tx_i}}{1+e^{w^T{x_i}}}$。而单个样本的标签分类最终只能是$0$或者$1$。")
+st.markdown("&emsp;&emsp;因此，可以用公式$p(y_{i}=1|x_{i};w)^{y_{i}}*p(y_{i}=0|x_{i};w)^{1-y_{i}}$对样本的预测信息进行统一描述，当$y_{i}=1$时，\
+            公式退化为$p(y_{i}=1|x_{i};w)$,当$y_{i}=0$时，公式退化为$p(y_{i}=0|x_{i};w)$。根据极大似然估计，将样本的对数似然最大化，先写出对数似然函数:")
+st.latex(r'''\begin{aligned} lnP&=ln\prod_{i=1}^{n}{p(y_{i}=1|x_{i};w)^{y_{i}}*p(y_{i}=0|x_{i};w)^{1-y_{i}}} \\ 
+&=\Sigma_{i=1}^{n}{y_{i}lnp(y_{i}=1|x_{i};w)+(1-y_{i})lnp(y_{i}=0|x_{i};w)} \\
+&=\Sigma_{i=1}^{n}y_{i}ln\cfrac{e^{w^Tx_{i}}}{1+e^{w^Tx_{i}}}+(1-y_{i})ln\cfrac{1}{1+e^{w^Tx_{i}}}  \\
+&=\Sigma_{i=1}^{n}y_{i}(w^Tx_{i}-ln(1+e^{w^Tx_{i}}))-(1-y_{i})ln(1+e^{w^Tx_{i}})  \\ 
+&=\Sigma_{i=1}^{n}y_{i}w^Tx_{i}-ln(1+e^{w^Tx_{i}})\end{aligned} \tag {1.2}''')
+st.markdown("&emsp;&emsp;寻求的最优参数$w$就相当于找一个$w$使得似然函数最大,即：")
+st.latex(r''' \begin{aligned}\tilde{w}&=\arg \mathop{\max}\limits_{w}\Sigma_{i=1}^{n}~y_{i}w^Tx_{i}-ln(1+e^{w^Tx_{i}}) \\
+ &=\arg\mathop{\min}\limits_{w}\Sigma_{i=1}^{n}~ln(1+e^{w^Tx_{i}})-y_{i}w^Tx_{i}\end{aligned} \tag{1.3}''')
--- streamlit_demo.py
+++ streamlit_demo.py
@@ -1,48 +1,68 @@
 import os
 import streamlit as st
+import time,requests
+import numpy as np
+from config.constants import PHQ_9
 
 IMAGE_PATH="src/ML.jpeg"
-IMAGE_PATH1="src/Instruction_tuning.gif"
+IMAGE_PATH1="src/Psychology.png"
 IMAGE_PATH2="src/prml.png"
-st.title(":blue[机器学习理论与实践] 🎈")
+IMAGE_PATH3="src/ml_whiteboard.png"
+IMAGE_PATH4="src/splg.png"
+IMAGE_PATH5="src/sml.png"
+st.title("🌟 :blue[探索心理学与机器学习的奇妙交叉领域]🌟 ")
 st.sidebar.markdown("# Main page 🎈")
-st.title(":blue[Welcome to my space~]")
+
+st.markdown("&emsp;&emsp;欢迎来到🧠心大陆空间。我们致力于将心理学与机器学习结合，探索这两个领域的奇妙交叉点。\
+    无论你是对心理学或机器学习感兴趣，还是想了解二者的共同之处，我们都为你准备了丰富的内容和深入的研究。\
+    在这里，你将逐步了解将传统的统计机器学习方法、自然语言处理算法以及这些算法的实战演练，在学习的过程中你将会逐渐对这些算法有深刻的理解~")
+
 st.image(IMAGE_PATH)
 st.image(IMAGE_PATH1)
 # 在正文区域创建一个块级列表
-st.markdown("## 本文主要参考教材如下：")
+st.markdown("### 本文主要参考教材如下：")
 
 options = [
-    "[pattern recognition and machine learning](https://www.microsoft.com/en-us/research/uploads/prod/2006/01/Bishop-Pattern-Recognition-and-Machine-Learning-2006.pdf)",
-    "[统计学习方法](https://github.com/fengdu78/lihang-code)",
-    "[机器学习白板推导系列](https://space.bilibili.com/97068901)",
-    "[speech and language processing](https://web.stanford.edu/~jurafsky/slp3/)"
+    "🚀 [pattern recognition and machine learning](https://www.microsoft.com/en-us/research/uploads/prod/2006/01/Bishop-Pattern-Recognition-and-Machine-Learning-2006.pdf)",
+    "🔮[统计学习方法](https://github.com/fengdu78/lihang-code)",
+    "⚡️[机器学习白板推导系列](https://space.bilibili.com/97068901)",
+    "🌐[speech and language processing](https://web.stanford.edu/~jurafsky/slp3/)"
 ]
 
 for i,option in enumerate(options):
+    st.markdown(f"##### {option}")
     
-    st.markdown(f"- {option}")
-    if i==0:
-        st.image(IMAGE_PATH2)
-    
-    
-    
-    
-button_home = st.button("首页")
-button_products = st.button("产品")
-button_solutions = st.button("解决方案")
+option_list=[]
+sum_score=0
+state=False
+with st.expander(':orange[抑郁症筛查量表（PHQ-9）测评体验]'):
+    st.markdown("###### &emsp;&emsp;请根据最近两个星期内您的实际感受，即以下症状在您的生活中出现的频率有多少？\
+        选择一个与您的情况最符合的答案，不要花费太多时间去思考，根据第一印象做出判断，答案没有对错，真实反应自己的感受就好。")
+    with st.form('my_form'):
 
-# 根据按钮点击情况显示内容
-if button_home:
-    st.header("欢迎来到首页！")
-    # 显示首页内容...
-
-if button_products:
-    st.header("这是我们的产品页面！")
-    # 显示产品页面内容...
-
-if button_solutions:
-    st.header("这是我们的解决方案页面！")
+        for index,question in enumerate(PHQ_9["Project"]):
+            option_list.append(st.selectbox(":yellow[{}.{}]".format((index+1),question),PHQ_9["options"]))
+            
+        submitted = st.form_submit_button('Submit')
+            
+    score_list=[]
+    if submitted:
+        for opt in option_list:
+                score_list.append(PHQ_9["score_map"][opt])
+        sum_score=sum(score_list)
+        st.success(f'''恭喜你完成填写，您的得分是`{sum_score}`!''', icon="✅")
+        state=True
+        #st.markdown(f'''##### 恭喜你成功完成填写,你的得分是`{sum_score}`''')
+if state:
+    with st.expander("请查看专业报告~"):    
+            if sum_score>10:
+                st.write("由于你的得分大于10，所以你有一点抑郁情绪")
+            else:
+                st.write("你很健康噢")
+button_option_list=[]
+                
+            
+            
 if __name__=="__main__":
     pass
 
